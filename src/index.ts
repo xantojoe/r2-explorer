2dddfcb4c7650e8c8cd3eedc37d3d8a570cc2cbf
Xanto <15924332+xantojoe@users.noreply.github.com>
2025-08-29 10:27:41 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { R2Explorer } from "r2-explorer";
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: true,
+// export default R2Explorer({
+//   // Set to false to allow users to upload files
+//   readonly: true,
 
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
+//   // Learn more how to secure your R2 Explorer instance:
+//   // https://r2explorer.com/getting-started/security/
+//   // cfAccessTeamName: "my-team-name",
+// });
+export default R2Explorer({ readonly: false, basicAuth: [{
+    username: 'xdzzz',
+    password: 'ak2325938'
+  }]
 });
